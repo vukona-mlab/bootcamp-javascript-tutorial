1911106c5d72db38aa4b36cc22e416ca4fb70a20
vukona <vukona@mlab.co.za>
2024-04-16 18:00:43 +0200

    added array methods

--- index.js
+++ index.js
@@ -2,3 +2,22 @@
 let listOfStudents = ['Vukona', 'Kabelo', 'Refilwe', 'Zack', 'Nokwanda']
 console.log({ listOfStudents });
 console.log(listOfStudents.length );
+
+// add item onto array
+listOfStudents.push('Kgaugelo')
+console.log({ listOfStudents });
+
+// remove item from an array
+listOfStudents.splice(0, 1)
+console.log(listOfStudents);
+
+listOfStudents[0] = "Kaybee"
+console.log(listOfStudents);
+
+listOfStudents.forEach(item => {
+    console.log('forEach: ', item);
+})
+
+for(let i = 0; i <= listOfStudents.length - 1; i++) {
+    console.log('for loop: ', listOfStudents[i])
+}
