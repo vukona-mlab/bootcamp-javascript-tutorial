7c67918b3be98c868d611f1976e2ce3ed97e3ff0
vukona <vukona@mlab.co.za>
2024-04-16 17:52:42 +0200

    just intro to different functions

--- index.js
+++ index.js
@@ -1,88 +1,38 @@
-// working mathematical operators
-// +, -, /, *
-// string concatenator
-console.log("hi there" + " my name is Vukona");
-
-// number addition
-console.log(3 + 5);
-
-// conversion: explicit conversion by the programmer
-// coercion: implicit conversion by JavaScript
-
-// direct conversion
-console.log(String(3) + String(5));
-console.log(Number("5") + Number("9"));
-
-// implicit conversion
-console.log(5 + 10 + '4');
-
-// subtract
-let firstNumber = 100;
-let secondNumber = 50
-let remainder = firstNumber - secondNumber
-
-console.log(remainder);
-console.log(secondNumber - firstNumber);
-
-// division
-console.log(firstNumber / secondNumber);
-console.log(secondNumber / firstNumber);
-
-// multiplication
-
-console.log(firstNumber * 8);
-let product = firstNumber * secondNumber
-let newNumber = product / 4
-console.log(product);
-console.log(newNumber);
-
-// Logical operators
-// &&, ||, ==, ===, !==
-
-// if statements
-if(5 === 5) {
-    console.log(true);
-
-} else {
-    console.log(false);
+count('Vukona')
+function count(user) { // you need to understand hoisting 
+    // function declaration
+    // call it before declaration
+    console.log('hi there ' + user);
 }
 
-if(firstNumber > secondNumber) {
-    console.log('first number is higher');
-} else {
-    console.log('second number is higher');
+// console.log();
+
+const countToo = function(user) {
+    // function expression
+    // can't call it before declaration
+    console.log('hi there ' + user);
+}
+countToo('Mnisi')
+
+// just logging
+const countThree = (a, b) => {
+    console.log('hi Vee');
+    console.log(a + b);
+}
+countThree(3, 6)
+
+// expecting a return value
+const countNumbers = (a, b) => a + b
+let sumOfNumbers = countNumbers(3, 6)
+console.log('total: ', sumOfNumbers);
+
+function addSum(a, b) {
+    return a + b
+}
+function returnGreeting(personName) {
+    return "Hello " + personName
 }
 
-// both conditions are met
-if((firstNumber > secondNumber) && (remainder < product)) {
-    console.log('both conditions were met');
-} else {
-    console.log('One or both conditions were not met');
-}
+console.log('addSum total: ', addSum(3, 18));
 
-// One condition is met
-if((firstNumber > secondNumber) && (remainder > product)) {
-    console.log('both conditions were met');
-} else {
-    console.log('One or both conditions were not met');
-}
-
-// both conditions are not met
-if((firstNumber < secondNumber) && (remainder > product)) {
-    console.log('both conditions were met');
-} else {
-    console.log('One or both conditions were not met');
-}
-
-// checking if at least one of the conditions is true
-if((firstNumber < secondNumber) || (remainder > product)) {
-    console.log('at least one condition met: OR');
-} else {
-    console.log('Both conditions were not met: OR');
-}
-// Checking if not equals
-if(firstNumber !== 100) {
-    console.log("numbers don't match");
-} else {
-    console.log("numbers do match");
-}
+console.log(returnGreeting("Kgaugelo"));
